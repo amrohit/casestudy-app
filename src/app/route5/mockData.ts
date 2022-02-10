9b024786c7ed72b395f79ff6ad29d7b547b7f746
Rohit Sharma <35895869+amrohit@users.noreply.github.com>
2022-02-11 00:03:17 +0530

    task5, route5 complted

---
+++ src/app/route5/mockData.ts
@@ -0,0 +1,34 @@
+export const mockData = [
+  {
+    name: "Rohit",
+    class: 3,
+    section: "D",
+    sub1: 23,
+    sub2: 24,
+    sub3: 67,
+  },
+  {
+    name: "Mohit",
+    class: 2,
+    section: "A",
+    sub1: 53,
+    sub2: 44,
+    sub3: 67,
+  },
+  {
+    name: "Meera",
+    class: 5,
+    section: "E",
+    sub1: 73,
+    sub2: 64,
+    sub3: 37,
+  },
+  {
+    name: "Priya",
+    class: 9,
+    section: "B",
+    sub1: 93,
+    sub2: 84,
+    sub3: 97,
+  },
+];
--- src/app/route5/route5.component.ts
+++ src/app/route5/route5.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { mockData } from './mockData';
+
+interface IMockResponse {
+  name: string;
+  class: number;
+  section: string;
+  sub1: number;
+  sub2: number;
+  sub3: number;
+}
 
 @Component({
   selector: 'app-route5',
   templateUrl: './route5.component.html',
-  styleUrls: ['./route5.component.scss']
+  styleUrls: ['./route5.component.scss'],
 })
 export class Route5Component implements OnInit {
-
-  constructor() { }
+  mockApiResponse: IMockResponse[] = [];
+  clickCount = 0;
+  lastKey = '';
+  constructor() {}
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  getData() {
+    this.mockApiResponse = [...mockData];
+  }
+
+  sortBy(key: string) {
+    if (key !== this.lastKey && this.lastKey !== '') {
+      this.clickCount = 0;
+      this.getData();
+      this.lastKey = key;
+      return;
+    }
+    if (this.clickCount === 0) {
+      this.mockApiResponse.sort((a: any, b: any) => a[key] - b[key]);
+      this.clickCount = this.clickCount + 1;
+    } else if (this.clickCount === 1) {
+      this.mockApiResponse.sort((a: any, b: any) => b[key] - a[key]);
+      this.clickCount = this.clickCount + 1;
+    } else {
+      this.clickCount = 0;
+      this.getData();
+    }
+    this.lastKey = key;
+  }
 }
